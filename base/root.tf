045969300715c8d4600db251dc8eb86115c13efd
Artem Zaporogskiy <artem.zaporogskiy@gmail.com>
2024-08-01 00:21:13 +0400

    Terraform was initialized.

---
+++ base/root.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 5.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+}

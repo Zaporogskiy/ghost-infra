82311feeaf3d0e4ba508591f4784f86bb4f4b9ae
Artem Zaporogskiy <artem.zaporogskiy@gmail.com>
2024-08-05 23:29:52 +0400

    ALB was configured.

--- base/network.tf
+++ base/network.tf
@@ -65,3 +65,76 @@ resource "aws_route_table_association" "public_c_association" {
   subnet_id      = aws_subnet.public_c.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_lb" "alb_ghost" {
+  name               = "alb-ghost"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id, aws_subnet.public_c.id]
+  security_groups    = [aws_security_group.alb.id]
+
+  tags = merge(local.tags, {
+    Name = "alb-ghost"
+  })
+}
+
+resource "aws_lb_target_group" "ghost_ec2" {
+  name     = "ghost-ec2"
+  port     = 2368
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.cloudx.id
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = merge(local.tags, {
+    Name = "ghost-ec2"
+  })
+}
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.alb_ghost.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ghost_ec2.arn
+  }
+  tags = local.tags
+}
+
+resource "aws_lb_listener_rule" "listener_rule" {
+  listener_arn = aws_lb_listener.alb_listener.arn
+  priority     = 100
+
+  action {
+    type = "forward"
+
+    forward {
+      target_group {
+        arn    = aws_lb_target_group.ghost_ec2.arn
+        weight = 100
+      }
+
+      stickiness {
+        enabled  = true
+        duration = 300
+      }
+    }
+  }
+
+  condition {
+    source_ip {
+      values = ["${chomp(data.http.myip.response_body)}/32"]
+    }
+  }
+  tags = local.tags
+}

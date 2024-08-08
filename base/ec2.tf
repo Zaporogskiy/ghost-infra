94ab7e4089bc3ae605e0934526598948d8654d6d
Artem Zaporogskiy <artem.zaporogskiy@gmail.com>
2024-08-08 12:12:18 +0400

    Refactoring of the user_data block.

--- base/ec2.tf
+++ base/ec2.tf
@@ -21,65 +21,7 @@ resource "aws_launch_template" "ghost_launch_template" {
     create_before_destroy = true
   }
 
-  user_data = base64encode(<<-EOF_USER_DATA
-#!/bin/bash -xe
-
-exec > >(tee /var/log/cloud-init-output.log | logger -t user-data -s 2>/dev/console) 2>&1
-### Update this to match your ALB DNS name
-LB_DNS_NAME="${aws_lb.alb_ghost.dns_name}"
-###
-
-REGION=$$(/usr/bin/curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/[a-z]$$//')
-EFS_ID=$$(aws efs describe-file-systems --query 'FileSystems[?Name==\`ghost_content\`].FileSystemId' --region $$REGION --output text)
-
-### Install pre-reqs
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
-yum install -y nodejs amazon-efs-utils
-npm install ghost-cli@latest -g
-
-adduser ghost_user
-usermod -aG wheel ghost_user
-cd /home/ghost_user/
-
-sudo -u ghost_user ghost install --version 4.12.1 --local
-
-### EFS mount
-mkdir -p /home/ghost_user/ghost/content/data
-mount -t efs -o tls $$EFS_ID:/ /home/ghost_user/ghost/content
-
-cat > /home/ghost_user/config.development.json <<-EOF_CONFIG
-{
-  "url": "http://$$LB_DNS_NAME",
-  "server": {
-    "port": 2368,
-    "host": "0.0.0.0"
-  },
-  "database": {
-    "client": "sqlite3",
-    "connection": {
-      "filename": "/home/ghost_user/ghost/content/data/ghost-local.db"
-    }
-  },
-  "mail": {
-    "transport": "Direct"
-  },
-  "logging": {
-    "transports": [
-      "file",
-      "stdout"
-    ]
-  },
-  "process": "local",
-  "paths": {
-    "contentPath": "/home/ghost_user/ghost/content"
-  }
-}
-EOF_CONFIG
-
-sudo -u ghost_user ghost stop
-sudo -u ghost_user ghost start
-EOF_USER_DATA
-  )
+  user_data = base64encode(file("${path.module}/user_data.sh"))
 }
 
 resource "aws_autoscaling_group" "ghost_ec2_pool" {
--- base/network_security.tf
+++ base/network_security.tf
@@ -62,20 +62,18 @@ resource "aws_security_group" "ec2_pool" {
   }
 
   ingress {
-    from_port                = 2368
-    to_port                  = 2368
-    protocol                 = "tcp"
+    from_port       = 2368
+    to_port         = 2368
+    protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
   }
-}
 
-resource "aws_security_group_rule" "ec2_pool_egress" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.ec2_pool.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "efs" {

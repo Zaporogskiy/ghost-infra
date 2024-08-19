65a46786b47b213a06feff26ea0ee7a391d63cdd
Artem Zaporogskiy <artem.zaporogskiy@gmail.com>
2024-08-19 20:52:31 +0400

    Outputs were added.

---
+++ base/output.tf
@@ -0,0 +1,11 @@
+output "my_ip" {
+  value = chomp(data.http.myip.response_body)
+}
+
+output "alb_dns_name" {
+  value = aws_lb.alb_ghost.dns_name
+}
+
+output "user_data" {
+  value = aws_launch_template.ghost_launch_template.user_data
+}

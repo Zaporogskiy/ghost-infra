eb8b15560d7ab49dbeb70298a8f71fa12491ee3d
Artem Zaporogskiy <artem.zaporogskiy@gmail.com>
2024-08-27 16:23:55 +0400

    Private subnets were added.

--- base/locals.tf
+++ base/locals.tf
@@ -3,6 +3,9 @@ locals {
   public_subnet_1     = "${var.environment}-${var.project_name}-${var.region}-public-001"
   public_subnet_2     = "${var.environment}-${var.project_name}-${var.region}-public-002"
   public_subnet_3     = "${var.environment}-${var.project_name}-${var.region}-public-003"
+  private_subnet_1     = "${var.environment}-${var.project_name}-${var.region}-private-001"
+  private_subnet_2     = "${var.environment}-${var.project_name}-${var.region}-private-002"
+  private_subnet_3     = "${var.environment}-${var.project_name}-${var.region}-private-003"
 
   tags = {
     Project = var.project_name
--- base/network.tf
+++ base/network.tf
@@ -8,6 +8,14 @@ resource "aws_vpc" "cloudx" {
   })
 }
 
+resource "aws_internet_gateway" "cloudx_igw" {
+  vpc_id = aws_vpc.cloudx.id
+
+  tags = merge(local.tags, {
+    Name = "cloudx-igw"
+  })
+}
+
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.cloudx.id
   cidr_block              = "10.10.1.0/24"
@@ -38,11 +46,33 @@ resource "aws_subnet" "public_c" {
   })
 }
 
-resource "aws_internet_gateway" "cloudx_igw" {
-  vpc_id = aws_vpc.cloudx.id
-
+resource "aws_subnet" "private_db_a" {
+  vpc_id                  = aws_vpc.cloudx.id
+  cidr_block              = "10.10.20.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
   tags = merge(local.tags, {
-    Name = "cloudx-igw"
+    Name = local.private_subnet_1
+  })
+}
+
+resource "aws_subnet" "private_db_b" {
+  vpc_id                  = aws_vpc.cloudx.id
+  cidr_block              = "10.10.21.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+  tags = merge(local.tags, {
+    Name = local.private_subnet_2
+  })
+}
+
+resource "aws_subnet" "private_db_c" {
+  vpc_id                  = aws_vpc.cloudx.id
+  cidr_block              = "10.10.22.0/24"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = false
+  tags = merge(local.tags, {
+    Name = local.private_subnet_3
   })
 }
 
@@ -55,6 +85,14 @@ resource "aws_route_table" "public_rt" {
 
   tags = merge(local.tags, {
     Name = "public_rt"
+  })
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.cloudx.id
+
+  tags = merge(local.tags, {
+    Name = "private_rt"
   })
 }
 
@@ -71,6 +109,21 @@ resource "aws_route_table_association" "public_b_association" {
 resource "aws_route_table_association" "public_c_association" {
   subnet_id      = aws_subnet.public_c.id
   route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_db_a_association" {
+  subnet_id      = aws_subnet.private_db_a.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_db_b_association" {
+  subnet_id      = aws_subnet.private_db_b.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_db_c_association" {
+  subnet_id      = aws_subnet.private_db_c.id
+  route_table_id = aws_route_table.private_rt.id
 }
 
 resource "aws_lb" "alb_ghost" {

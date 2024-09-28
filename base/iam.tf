4d9e227ed3f64fb5c86a25b80211d0f831e996c2
Artem Zaporogskiy <artem.zaporogskiy@gmail.com>
2024-09-28 23:53:53 +0400

    Configured IAM role for ecs. Dockerfile was added for building an image.

--- base/iam.tf
+++ base/iam.tf
@@ -62,3 +62,49 @@ resource "aws_iam_instance_profile" "ghost_app" {
   role = aws_iam_role.ghost_app_role.name
   tags = local.tags
 }
+
+resource "aws_iam_role" "ghost_ecs_role" {
+  name = "ghost_ecs"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+  tags = local.tags
+}
+
+resource "aws_iam_role_policy" "ghost_ecs_policy" {
+  name = "ghost_ecs_policy"
+  role = aws_iam_role.ghost_ecs_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "elasticfilesystem:DescribeFileSystems",
+          "elasticfilesystem:ClientMount",
+          "elasticfilesystem:ClientWrite"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ghost_ecs" {
+  name = "ghost_ecs"
+  role = aws_iam_role.ghost_ecs_role.name
+  tags = local.tags
+}
